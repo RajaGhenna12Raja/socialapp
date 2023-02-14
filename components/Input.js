f412042ce3800ad3ad7048f31673e1c9aaa1b83f
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-02-15 02:56:12 +0530

    added image and pulse ieffect

--- components/Input.js
+++ components/Input.js
@@ -15,67 +15,73 @@ const [loading, setLoading] = useState(false)
 const filePickerRef = useRef(null)
 
 const sendPost = async ()=>{
-     if ( loading ) return;
-     setLoading(true)
+if ( loading ) return;
+setLoading(true)
 
-    const docRef = await addDoc( collection (db, "posts"), {
-           id: session.user.uid,
-           text: input,
-           userImg: session.user.image,
-           timestamp: serverTimestamp(),
-           name: session.user.name,
-           username: session.user.username,  
-    })
-    const imageRef = ref (storage, `posts/${docRef.id}/image`)
-    if (selectedFile){
-        await uploadString(imageRef, selectedFile, "data_url").then(async() =>{
-            const downloadURL = await getDownloadURL (imageRef)
-            await updateDoc (doc(db, "posts",  docRef.id),{
-               image: downloadURL ,
-            })
-        })
-    }
-    setInput('');
-    setSelectedFile('');
-    setLoading(false);
+const docRef = await addDoc( collection (db, "posts"), {
+id: session.user.uid,
+text: input,
+userImg: session.user.image,
+timestamp: serverTimestamp(),
+name: session.user.name,
+username: session.user.username,  
+})
+const imageRef = ref (storage, `posts/${docRef.id}/image`)
+if (selectedFile){
+await uploadString(imageRef, selectedFile, "data_url").then(async() =>{
+const downloadURL = await getDownloadURL (imageRef)
+await updateDoc (doc(db, "posts",  docRef.id),{
+image: downloadURL ,
+})
+})
+}
+setInput('');
+setSelectedFile('');
+setLoading(false);
 };
 const addImageToPost= (e) => {
-   const reader = new FileReader();
-   if (e.target.files[0]) {
-    reader.readAsDataURL(e.target.files[0]);
-   }
-   reader.onload = (readerEvent)=>{
-    setSelectedFile(readerEvent.target.result);
-   }
+const reader = new FileReader();
+if (e.target.files[0]) {
+reader.readAsDataURL(e.target.files[0]);
+}
+reader.onload = (readerEvent)=>{
+setSelectedFile(readerEvent.target.result);
+}
 }
 return (
 <>
 {session  &&  (
-    <div className='flex border-b border-gray-300 p-3 space-x-3'>
-    <Image onClick={signOut} className='rounded-full cursor-pointer hover:brightness-95'  src={session.user.image}  alt="userImage"  height={50} width={50}/>
-    <div className='w-full divide-y divide-gray-300'>
-    <div className=''>
-        <textarea value={input} onChange={(e) =>setInput(e.target.value)} className='w-full border-none focus:ring-0 text-lg  placeholder:gray-700 tracking-wide min-h-[50px] text-gray-700 ' rows='2'  placeholder='Share Your Thoughts'/>
-    </div>
-    <div className='flex items-center justify-between pt-2.5 '>
-            <div className='flex'>
-                <div className='' onClick={() =>filePickerRef.current.click()}>
-                <PhotographIcon  className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
-                  <input type='file' hidden ref={filePickerRef} onChange={addImageToPost}/>
-                </div>
-                {selectedFile && (
-                    <div className='relative'>
-                        <XIcon onClick={() => setSelectedFile(null)}  className='h-6 w-6 absolute cursor-pointer rounded-full bg-red-600 text-white hover:bg-black p-1'/>
-                        <Image src={selectedFile} className={`${loading && "animate-pulse"}`} height={300} width={300}/>
-                    </div>
-                ) }
-            <EmojiHappyIcon className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
-            </div>
-            <button onClick={sendPost} disabled={!input.trim()} className='bg-red-600 text-white px-4 py-1.5 rounded-full hover:bg-black font-bold shadow-md  disabled:opacity-50'>Tweet</button>
-    </div>
-    </div>
-    </div>
-    )}
+<div className='flex border-b border-gray-300 p-3 space-x-3'>
+<Image onClick={signOut} className='rounded-full cursor-pointer hover:brightness-95'  src={session.user.image}  alt="userImage"  height={50} width={50}/>
+<div className='w-full divide-y divide-gray-300'>
+<div className=''>
+<textarea value={input} onChange={(e) =>setInput(e.target.value)} className='w-full border-none focus:ring-0 text-lg  placeholder:gray-700 tracking-wide min-h-[50px] text-gray-700 ' rows='2'  placeholder='Share Your Thoughts'/>
+</div>
+{selectedFile && (
+<div className='relative'>
+<XIcon onClick={() => setSelectedFile(null)}  className='h-6 w-6 absolute cursor-pointer rounded-full bg-red-600 text-white hover:bg-black p-1'/>
+<Image src={selectedFile} className={`${loading && "animate-pulse"}`} height={300} width={300}/>
+</div>
+) }
+<div className='flex items-center justify-between pt-2.5 '>
+{!loading && (
+<>
+<div className='flex'>
+<div className='' onClick={() =>filePickerRef.current.click()}>
+<PhotographIcon  className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
+<input type='file' hidden ref={filePickerRef} onChange={addImageToPost}/>
+</div>
+<EmojiHappyIcon className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
+</div>
+<button onClick={sendPost} disabled={!input.trim()} className='bg-red-600 text-white px-4 py-1.5 rounded-full hover:bg-black font-bold shadow-md  disabled:opacity-50'>
+Tweet
+</button>
 </>
- )
+)}
+</div>
+</div>
+</div>
+)}
+</>
+)
 }
