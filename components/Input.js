6bc5fdd849d2cd434bd150656449bcd96d9a2c97
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-02-15 02:05:47 +0530

    uploaded image to database
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- components/Input.js
+++ components/Input.js
@@ -1,28 +1,53 @@
 import React from 'react'
 import Image from 'next/image'
-import { EmojiHappyIcon, PhotographIcon } from '@heroicons/react/solid'
+import { EmojiHappyIcon, PhotographIcon, XIcon } from '@heroicons/react/solid'
 import {useSession, signOut} from "next-auth/react"
-import { useState } from 'react'
-import {db} from "../firebase"
-import { addDoc, collection, serverTimestamp} from "firebase/firestore"
+import { useState, useRef } from 'react'
+import {db, storage} from "../firebase"
+import { addDoc, collection,  doc, serverTimestamp, updateDoc} from "firebase/firestore"
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
 
 export const Input = () => {
-const {data: session} = useSession();
-const [input, setInput] = useState(" ");
+const {data: session} = useSession()
+const [input, setInput] = useState(" ")
+const [selectedFile, setSelectedFile] = useState(null)
+const [loading, setLoading] = useState(false)
+const filePickerRef = useRef(null)
 
 const sendPost = async ()=>{
-    const docRef = await addDoc(collection(db, "posts"), {
+     if ( loading ) return;
+     setLoading(true)
+
+    const docRef = await addDoc( collection (db, "posts"), {
            id: session.user.uid,
            text: input,
            userImg: session.user.image,
            timestamp: serverTimestamp(),
            name: session.user.name,
-           username: session.user.username
+           username: session.user.username,  
     })
-
-    setInput("");
+    const imageRef = ref (storage, `posts/${docRef.id}/image`)
+    if (selectedFile){
+        await uploadString(imageRef, selectedFile, "data_url").then(async() =>{
+            const downloadURL = await getDownloadURL (imageRef)
+            await updateDoc (doc(db, "posts",  docRef.id),{
+               image: downloadURL ,
+            })
+        })
+    }
+    setInput('');
+    setSelectedFile('');
+    setLoading(false);
+};
+const addImageToPost= (e) => {
+   const reader = new FileReader();
+   if (e.target.files[0]) {
+    reader.readAsDataURL(e.target.files[0]);
+   }
+   reader.onload = (readerEvent)=>{
+    setSelectedFile(readerEvent.target.result);
+   }
 }
-
 return (
 <>
 {session  &&  (
@@ -34,7 +59,16 @@ return (
     </div>
     <div className='flex items-center justify-between pt-2.5 '>
             <div className='flex'>
-            <PhotographIcon  className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
+                <div className='' onClick={() =>filePickerRef.current.click()}>
+                <PhotographIcon  className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
+                  <input type='file' hidden ref={filePickerRef} onChange={addImageToPost}/>
+                </div>
+                {selectedFile && (
+                    <div className='relative'>
+                        <XIcon onClick={() => setSelectedFile(null)}  className='h-6 w-6 absolute cursor-pointer rounded-full bg-red-600 text-white hover:bg-black p-1'/>
+                        <Image src={selectedFile} className={`${loading && "animate-pulse"}`} height={300} width={300}/>
+                    </div>
+                ) }
             <EmojiHappyIcon className='h-10 w-10 hoverEffect p-2 text-red-600 '/>
             </div>
             <button onClick={sendPost} disabled={!input.trim()} className='bg-red-600 text-white px-4 py-1.5 rounded-full hover:bg-black font-bold shadow-md  disabled:opacity-50'>Tweet</button>
